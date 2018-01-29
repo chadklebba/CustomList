11a86263ca826c7bb955430bf80289aa1dffe744
chadklebba <chad.klebba@gmail.com>
2018-01-29 14:09:11 -0600

    passed all of my add tests

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        public int count;
+        public T[] array;
+        public int capacity;
+        public T[] newarray;
+        public CustomList()
+        {
+            capacity = 1;
+            count = 0;
+            array = new T[capacity]; 
+        }
+        public void Add(T item)
+        {
+            capacity = capacity + 1;
+            newarray = new T[capacity];
+            count++;    
+            for (int i = 0; i <= count-1; i++)
+                {
+                    newarray[i] = array[i];
+                }
+                array = newarray;
+                array[count -1] = item;
+                     
+                
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            
+            
+        }
+
+        public void Remove(T item)
+        {
+
+        }
+    }
+}
---
+++ TestCustomList/TestCustomList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace TestCustomList
+{
+    [TestClass]
+    public class TestCustomList
+    {
+        [TestMethod]
+        public void Add_TwoInts_ListWithTwoInts()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            //Act
+            customList.Add(1);
+            customList.Add(2);
+            //Assert
+            Assert.AreEqual(customList.Count, 2);
+        }
+   
+        [TestMethod]
+        public void Add_TwoIntsToExistingList_ListWithFourInts()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            //Act
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            //Assert
+            Assert.AreEqual(customList.Count, 4);
+        }
+
+        [TestMethod]
+        public void Add_TwoIntsToExistingList_ThirdNumberSecondIndex()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            //Act
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            //Assert
+            Assert.AreEqual(customList.array[2], 3);
+        }
+
+        [TestMethod]
+        public void Add_TwoStrings_ListWithTwoStrings()
+        {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            //Act
+            customList.Add("One");
+            customList.Add("Two");
+            //Assert
+            Assert.AreEqual(customList.Count, 2);
+        }
+
+        [TestMethod]
+        public void Add_TwoStrings_ListWithFourStrings()
+        {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            //Act
+            customList.Add("One");
+            customList.Add("Two");
+            customList.Add("Three");
+            customList.Add("Four");
+            //Assert
+            Assert.AreEqual(customList.Count, 4);
+        }
+
+        [TestMethod]
+        public void Add_TwoStrings_ThirdStringSecondIndex()
+        {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            //Act
+            customList.Add("One");
+            customList.Add("Two");
+            customList.Add("Three");
+            customList.Add("Four");
+            //Assert
+            Assert.AreEqual(customList.array[2], "Three");
+        }
+
+        //[TestMethod]
+        //public void Remove_OneOfFourInts_ListWithThreeInts()
+        //{
+        //    //Arrange
+        //    CustomList<int> customList = new CustomList<int>() { 1, 2, 3, 4 };
+        //    //Act
+        //    customList.Remove(1);
+        //    //Assert
+        //    Assert.AreEqual(customList.Count, 3);
+        //}
+    }
+}
